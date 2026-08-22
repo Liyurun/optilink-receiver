d741828a0c5d0d9639a42c09913e448145f787a5
Liyurun <Liyurun@users.noreply.github.com>
2026-08-22 16:52:16 +0800

    Harden Safari receiver initialization

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'optilink-web-receiver-v2';
+const CACHE_NAME = 'optilink-web-receiver-v3';
 const APP_SHELL = ['./', './manifest.webmanifest', './icon-180.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
