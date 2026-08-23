dd26d6c337ab98c62f1e75230d669f0762d7999f
Li Yuruan <1404020204@s.upc.edu.cn>
2026-08-23 21:15:23 +0800

    [codex/gpt-5] Add stable QR broadcast receiver

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'optilink-web-receiver-v9';
+const CACHE_NAME = 'optilink-web-receiver-v10';
 const APP_SHELL = ['./', './manifest.webmanifest', './icon-180.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
